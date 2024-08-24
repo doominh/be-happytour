66ca8fbc810308725531386f12c31484789d24cd
Do Cong Minh <doominh20y@gmail.com>
2024-08-24 09:52:37 +0700

    update user routes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const ctrls = require("../controllers/user");
 const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
+const uploader = require("../config/cloudinary.config");
 
 router.post("/register", ctrls.register);
 router.put("/finalregister/:token", ctrls.finalRegister);
@@ -12,7 +13,7 @@ router.post("/forgotpassword", ctrls.forgotPassword);
 router.put("/resetpassword", ctrls.resetPassword);
 router.get("/", [verifyAccessToken, isAdmin], ctrls.getUsers);
 router.delete("/:userId", [verifyAccessToken, isAdmin], ctrls.deleteUser);
-router.put("/current", [verifyAccessToken], ctrls.updateUser);
+router.put("/current", verifyAccessToken, uploader.single('avatar'), ctrls.updateUser);
 router.put("/:uid", [verifyAccessToken, isAdmin ], ctrls.updateUserByAdmin);
 
 module.exports = router;
